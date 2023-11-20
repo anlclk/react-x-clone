87a10111b948dac9a181834baff787e4e9176cc5
Anıl Çelik <anlclk11@hotmail.com>
2023-11-20 20:05:22 +0300

    sol sidebarın more kısmında yeni çıkan css details open'ı kullandım.

--- src/layout/nav/nav.jsx
+++ src/layout/nav/nav.jsx
@@ -91,21 +91,44 @@ export default function Navbar() {
                     <h4>Profile</h4>
                 </div>
             </Link>
-            <Link to="/more" className="navbarLink">
-                <div className="navbarBox" style={{ width: '133px' }}>
+            <details open>
+                <summary>
                     <svg viewBox="0 0 24 24" width="24" height="24" className="navbarSvg">
                         <path 
                             fill="#fff"
                             d="M3.75 12c0-4.56 3.69-8.25 8.25-8.25s8.25 3.69 8.25 8.25-3.69 8.25-8.25 8.25S3.75 16.56 3.75 12zM12 1.75C6.34 1.75 1.75 6.34 1.75 12S6.34 22.25 12 22.25 22.25 17.66 22.25 12 17.66 1.75 12 1.75zm-4.75 11.5c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25S6 11.31 6 12s.56 1.25 1.25 1.25zm9.5 0c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25-1.25.56-1.25 1.25.56 1.25 1.25 1.25zM13.25 12c0 .69-.56 1.25-1.25 1.25s-1.25-.56-1.25-1.25.56-1.25 1.25-1.25 1.25.56 1.25 1.25z">
                         </path>
-                    </svg>                   
-                    <h4>More</h4>
-                </div>
-            </Link>
+                    </svg>
+                    <h4>More</h4>  
+                </summary>
+                <Link to="/bookmarks" className="navbarLink">
+                    <div className="details">
+                        <svg viewBox="0 0 24 24" width="24" height="24" className="navbarSvg">
+                            <path 
+                            fill="#fff"
+                            d="M4 4.5C4 3.12 5.119 2 6.5 2h11C18.881 2 20 3.12 20 4.5v18.44l-8-5.71-8 5.71V4.5zM6.5 4c-.276 0-.5.22-.5.5v14.56l6-4.29 6 4.29V4.5c0-.28-.224-.5-.5-.5h-11z">
+                            </path>
+                        </svg>
+                        <h4>Bookmarks</h4>
+                    </div>
+                </Link>
+                <h2>Settings</h2>
+            </details>
         </nav>
     );
 }
 
 
+{/* <Link to="/more" className="navbarLink">
+    <div className="navbarBox" style={{ width: '133px' }}>
+        <svg viewBox="0 0 24 24" width="24" height="24" className="navbarSvg">
+            <path 
+                fill="#fff"
+                d="M3.75 12c0-4.56 3.69-8.25 8.25-8.25s8.25 3.69 8.25 8.25-3.69 8.25-8.25 8.25S3.75 16.56 3.75 12zM12 1.75C6.34 1.75 1.75 6.34 1.75 12S6.34 22.25 12 22.25 22.25 17.66 22.25 12 17.66 1.75 12 1.75zm-4.75 11.5c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25S6 11.31 6 12s.56 1.25 1.25 1.25zm9.5 0c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25-1.25.56-1.25 1.25.56 1.25 1.25 1.25zM13.25 12c0 .69-.56 1.25-1.25 1.25s-1.25-.56-1.25-1.25.56-1.25 1.25-1.25 1.25.56 1.25 1.25z">
+            </path>
+        </svg>                   
+        <h4>More</h4>
+    </div>
+</Link> */}
 
 
