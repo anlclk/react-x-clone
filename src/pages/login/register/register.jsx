50eef7e44b6659ead31b069361dcd83b68d708c4
Anıl Çelik <anlclk11@hotmail.com>
2023-12-28 17:51:07 +0300

    kayıt başarılı yazısı eklendi.

--- src/pages/login/register/register.jsx
+++ src/pages/login/register/register.jsx
@@ -4,6 +4,7 @@ import { useRef, useState } from "react";
 export default function SignIn() {
     const imagePreview = useRef();
     const profilePicturesPreview = useRef();
+    const [register, setRegister] = useState(false);
 
     function previewImage(e) {
         const reader = new FileReader();
@@ -41,6 +42,9 @@ export default function SignIn() {
         );
         
         console.log(data);
+        if(data) {
+            setRegister(true)
+        }
         console.log(error);
         
 
@@ -58,29 +62,32 @@ export default function SignIn() {
     }
 
     return(
+        register ? ( <div className="registerCompleted"><h1>Kayıt Başarılı</h1><h2>Sign In tıklayarak giriş yapabilirsiniz</h2></div> ) : (
+            
             <form id="register" className="form" autoComplete="off" onSubmit={addUser}>
-                <div className="wallpaper">
-                    <img src="https://placehold.co/598x183" ref={imagePreview} alt="wallpaper" />
-                    <label className="wallpaperPreviewArea">
-                        <input type="file" className="wallpaperUpload" name="wallpaperImg" onChange={previewImage} accept="image/jpg, image/jpeg" id="wallpaperInput" />
-                    </label>
-                </div>
-                <div className="profilePictures">
-                    <div className="profilePictureRelative">
-                        <img src="https://placehold.co/377x377" ref={profilePicturesPreview} alt="profilePicture" />
-                        <label className="profilePicturesPreviewArea">
-                            <input type="file" name="profilePicturesUpload" onChange={previewProfilePictures} accept="image/jpg, image/jpeg" id="profilePicturesInput" />
-                        </label>
-                    </div>
-                </div>
-                <div className="userDetails">
-                    <input required name="username" type="text" placeholder="username" />
-                    <input required type="text" name="userdataname" placeholder="@userId"/>
-                    <input required type="text" name="bio" placeholder="bio" />
-                    <input required type="email" name="email" autoComplete="off" placeholder="e-mail"/>
-                    <input required type="password" name="password" placeholder="password"/>
-                    <button className="btnLogin" type="submit" >Kayıt ol</button>
-                </div>
-            </form>   
+               <div className="wallpaper">
+                   <img src="https://placehold.co/598x183" ref={imagePreview} alt="wallpaper" />
+                   <label className="wallpaperPreviewArea">
+                       <input type="file" className="wallpaperUpload" name="wallpaperImg" onChange={previewImage} accept="image/jpg, image/jpeg" id="wallpaperInput" />
+                   </label>
+               </div>
+               <div className="profilePictures">
+                   <div className="profilePictureRelative">
+                       <img src="https://placehold.co/377x377" ref={profilePicturesPreview} alt="profilePicture" />
+                       <label className="profilePicturesPreviewArea">
+                           <input type="file" name="profilePicturesUpload" onChange={previewProfilePictures} accept="image/jpg, image/jpeg" id="profilePicturesInput" />
+                       </label>
+                   </div>
+               </div>
+               <div className="userDetails">
+                   <input required name="username" type="text" placeholder="username" />
+                   <input required type="text" name="userdataname" placeholder="@userId"/>
+                   <input required type="text" name="bio" placeholder="bio" />
+                   <input required type="email" name="email" autoComplete="off" placeholder="e-mail"/>
+                   <input required type="password" name="password" placeholder="password"/>
+                   <button className="btnLogin" type="submit" >Kayıt ol</button>
+               </div>
+           </form> 
+        ) 
     );
 }
